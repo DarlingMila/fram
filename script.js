4a1616b867fcfe9eb6d5346f0fe38953ae5442fa
Ludmila Gubanova <darlingmila@yandex.ru>
2023-06-23 14:27:05 +0300

    add comments, modal instructions

--- script.js
+++ script.js
@@ -4,17 +4,49 @@ const apiUrl =
 const numberOfTests = 26; //количестов тестов без Колеса (только номерные тесты). ---при необходимости изменить
 const numberOfExercises = 3; //количестов упражнений. ---при необходимости изменить
 
-let testsNames = [];
+// ключи - название упражнений, далее массив с правилами. В массиве строки/абзацы - это элементы массива
+const exerciseInstructions = {
+  "У1": [
+    "Упражнение 'Быть-Делать-Иметь'",
+    "Надо делать вот так -- 1",
+    "Так не надо делать -- 1",
+    "И третий блок для теста! -- 1",
+  ],
+  "У2": [
+    "Упражнение 'Опыт'",
+    "Надо делать вот так -- 2",
+    "Так не надо делать -- 2",
+    "И третий блок для теста! -- 2",
+  ],
+  "У3": [
+    "Упражнение 'Значимый опыт'",
+    "Надо делать вот так -- 3",
+    "Так не надо делать -- 3",
+    "И третий блок для теста! -- 3",
+  ],
+  "Колесо баланса": [
+    "Упражнение 'Колесо баланса'",
+    "Надо делать вот так -- КБ",
+    "Так не надо делать -- КБ",
+    "И третий блок для теста! -- КБ",
+  ],
+};
+
+let testsNames = []; // массив, куда функция ниже добавит необходимое количество листов, включая тесты, упражнения (+КБ и КБ результат), приветствие и прощанеи 
 
 for (let i = 0; i <= numberOfTests + numberOfExercises; i++) {
+
   switch (i) {
+
     case 0:
       testsNames.push(["Введение"]);
       break;
+
     case numberOfTests + numberOfExercises:
       testsNames.push([`У${i - numberOfTests}`], ["Колесо баланса"], ["Колесо: результат"], ["Заключение"]);
       testsNames = testsNames.flat();
       break;
+
     default:
       if (i <= numberOfTests) {
         testsNames.push([String(i)]);
@@ -25,39 +57,7 @@ for (let i = 0; i <= numberOfTests + numberOfExercises; i++) {
   }
 }
 
-console.log(testsNames);
-
-// const testsNames = [
-//   "Введение",
-//   "1",
-//   "2",
-//   "3",
-//   "4",
-//   "5",
-//   "6",
-//   "7",
-//   "8",
-//   "9",
-//   "10",
-//   "11",
-//   "12",
-//   "13",
-//   "14",
-//   "15",
-//   "16",
-//   "17",
-//   "18",
-//   "19",
-//   "20",
-//   "21",
-//   "22",
-//   "23",
-//   "24",
-//   "25",
-//   "Колесо баланса",
-//   "Колесо: результат", - при 25 тестах это будет 27 всего тестов те КР будет вся полоска прогресса закрашена
-//   "Заключение",
-// ];
+console.log(testsNames); // пока оставила, чтобы всегда была возможность отследить их до начала работы
 
 const sections = document.querySelectorAll(".section");
 const emailForm = document.querySelector("#emailForm");
@@ -74,131 +74,233 @@ const popupText = document.querySelector(".popup__text");
 const tableBlock = document.querySelector(".tableBlock");
 const progress = document.querySelector(".progress");
 
+const instructionModal = document.querySelector(".instructionModal");
+const instrCloseBtn = instructionModal.querySelector(".instructionModal__btn");
+const instrTextBlock = instructionModal.querySelector(".instructionModal__text");
+const instrOpenBtn = document.querySelector(".instrBtn");
+
+
+/**
+ * Функция закрывает попап с ошибкой, меняя классы с анимацией
+ */
 function closePopup() {
   popup.classList.remove("popup_show");
   popup.classList.add("popup_hide");
 }
 
+/**
+ * Функция откывает попап с ошибкой, меняя классы с анимацией
+ * @param {string} text - текст ошибки
+ * @param {boolean} onTest - булевое значение, располагается ли попап поверх теста
+ */
 function showPopup(text, onTest) {
   popupText.textContent = text;
   popup.classList.add("popup_show");
   popup.classList.remove("popup_hide");
 
+  /**
+   * Проверка: попап выпадает поверх теста
+   *    если да -> добавляется класс (меняются стили, чтобы не сливаться с фоном)
+   */
   if (onTest) popup.classList.add("popup_onTest");
 }
 
+/**
+ * Функция вызова или сокрытия скрина блокировщика, пока отправляются и загружаются данные
+ */
 function toggleTableBlock() {
   tableBlock.classList.toggle("tableBlock_hide");
 }
 
-
+/**
+ * Функция переключения между секциями. 
+ * Так как их всего две, то toggle просто добавляет класс одной и добавляет другой.
+ */
 function toggleSctions() {
   sections.forEach((section) => {
     section.classList.toggle("section_hide");
   });
 }
 
+/**
+ * Функция выхода из тестирования, которая также стирает данные из sessionStorage
+ */
+function exitTest() {
+  sessionStorage.clear();
+  toggleSctions();
+}
+
+/**
+ * Функция закрытия окна с инструкцией к упражнениям + добавления анимации к кнопке вызова
+ */
+function closeInstruction() {
+  instructionModal.classList.add("instructionModal_hide");
+  instrOpenBtn.classList.add("instrBtn_animation");
+
+  setTimeout(() => {
+    instructionModal.style.display = "none";
+  }, 150) // таймер ждет, пока пройден анимация ухода окна и затем полностью отключает блок
+}
+
+/**
+ * Функция открытия окна с инструкцией к упражнениям
+ */
+function showInstruction() {
+  instructionModal.style.display = "block";
+  instructionModal.classList.remove("instructionModal_hide");
+  instrOpenBtn.classList.remove("instrBtn_animation");
+}
+
+/**
+ * Функция для всех операций связанных с окном инструкций для упражнений
+ * @param {string} listName - название/номер теста или упажнения
+ * @returns boolean - ответ нужна ли инструкция
+ */
+function checkForInstructions(listName) {
+
+  /**
+   * Проверка: передано название упражнения - У_ или "Колесо баланса"
+   *    если да -> вызываются функции очистки и заполнения окна инструкции, его вызова и добавления кнопки
+   */
+  if (/^У\d+/.test(listName) || listName == "Колесо баланса") {
+    clearInstructionBlock();
+    addTextToInstruction(listName);
+    showInstruction();
+    instrOpenBtn.classList.remove("instrBtn_hide");
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Функция добавляет необходимый текст в инструкцию
+ * @param {string} listName - название или номер теста или упражнения 
+ */
+function addTextToInstruction(listName) {
+  const instructions = exerciseInstructions[listName];
+
+  instructions.forEach((text, i) => {
+    // если i == 0 значит это заголовок, остальное просто параграфы
+    const el = i == 0 ? "h3" : "p";
+    const cl = i == 0 ? "instructionModal__heading" : "instructionModal__par";
+
+    const item = createAndInsertText(el, cl, text);
+    instrTextBlock.appendChild(item);
+  })
+}
+
+/**
+ * Функция очистки. Цикл проверяет есть ди в блоке 1-ый элемент и если да, то удаляет его
+ */
+function clearInstructionBlock() {
+  while(instrTextBlock.firstChild) instrTextBlock.removeChild(instrTextBlock.firstChild);
+}
+
+/**
+ * Функция для создания и наполнения элементов
+ * @param {string} element - тэг
+ * @param {string} className - класс
+ * @param {string} text - текст
+ * @returns созданный элемент
+ */
+function createAndInsertText(element, className, text) {
+  const item = document.createElement(element);
+  item.classList.add(className);
+  item.textContent = text;
+  return item;
+}
+
+
+
+/**
+ * Асинхронная функция посылающая GET запрос: отправляет почту и получает данные, необходимые для тестирования
+ * @param {object} e - событие 
+ */
 async function openTests (e) {
   e.preventDefault();
 
+  // принудительное закрытие попапа, если он открыт
   if (popup.classList.contains("popup_show")) closePopup();
 
   const emailField = emailForm.querySelector("#email");
   const email = emailField.value.trim();
 
-  // alert(email);
-
   submitEmailBtn.disabled = true;
   submitEmailBtn.textContent = "Загружаем тесты...";
   emailField.readOnly = true;
 
+  /**
+   * При успешной отправки данных открывается тестирование, необходимый данные записываются в sessionStorage
+   * При неудаче всплывает окно с ошибкой
+   * В последнем блоке элементы возвращаются в прежнее состояния
+   */
   try {
     const response = await fetch(`${apiUrl}?email=${email}`);
     const res = await response.text();
 
-    console.log("res", res);
-
-    // alert(`openTests 
-    // try
-    // res:
-    // ${res}`);
-
-    // if (!res.includes("docs.google.com/spreadsheets")) {
-    //   return showPopup(res);
-    // }
-
     const data = res.split(",");
-    console.log(data)
 
-    // const url = `${data[0]}?rm=minimal&widget=true&headers=false`;
-    // &amp;
-    const url = `${data[0]}?rm=minimal`;
+    const url = `${data[0]}?rm=minimal`; //так таблица отображается без шапка
+
     const listIndex = data[1];
-    let listName = testsNames[listIndex];
-    listName = listName > numberOfTests ? `У${nextTest - numberOfTests}` : listName;
+    const listName = testsNames[listIndex];
 
     const amountOfTests = data[2];
 
-
+    //ниже все необходимые данные записываются в sessionStorage
     sessionStorage.setItem("email", email);
     sessionStorage.setItem("url", url);
     sessionStorage.setItem("listName", listName);
-
     sessionStorage.setItem("listIndex", listIndex);
     sessionStorage.setItem("amountOfTests", amountOfTests);
 
     setData(email, listName, url);
     emailField.value = "";
 
+    checkForInstructions(listName);
+    
   } catch (err) {
-
-    // alert(`openTests 
-    // catch
-    // err:
-    // ${err}
-    // err.message:
-    // ${err.message}`);
-
-    // console.log(err);
-    // console.log(err.message);
     showPopup("Некорректный адрес электронной почты");
 
   } finally {
-
     submitEmailBtn.disabled = false;
     submitEmailBtn.textContent = "Подтвердить";
     emailField.readOnly = false;
-    console.log("заканчиваем");
   }
 };
 
-function exitTest() {
-  sessionStorage.clear();
-  toggleSctions();
-}
-
-
-
+/**
+ * Асинхронная функция посылающая POST запрос: отправляет почту и получает данные, необходимые для тестирования
+ * @param {object} e - событие 
+ */
 async function submitTest (e) {
   e.preventDefault();
 
+  // принудительное закрытие попапа, если он открыт
   if (popup.classList.contains("popup_show")) closePopup();
+  instrOpenBtn.disabled = true;
 
   const listName = sessionStorage.getItem("listName");
-  if (listName === 'Заключение') return exitTest();
-  
+  if (listName === "Заключение") return exitTest();
+
   toggleTableBlock();
-  
+
   const url = sessionStorage.getItem("url");
   const email = sessionStorage.getItem("email");
-  // const newListName = testsNames[testsNames.indexOf(listName) + 1];
 
   submitTestBtn.disabled = true;
-  const btnText_onSubmit = getBtnText_onSubmit(listName)
+  const btnText_onSubmit = getBtnText_onSubmit(listName);
   submitTestBtn.textContent = btnText_onSubmit;
 
   let newListName = null;
 
+  /**
+   * При успешной отправки данных открывается новый лист
+   * При неудаче всплывает окно с ошибкой
+   * В последнем блоке элементы возвращаются в прежнее состояния, текст кнопки зависит от листа
+   */
   try {
     const response = await fetch(
       `${apiUrl}?url=${url}&listName=${listName}&email=${email}`,
@@ -209,57 +311,55 @@ async function submitTest (e) {
 
     const res = await response.text();
 
-    // alert(`submitTest 
-    // try
-    // res:
-    // ${res}`);
-
+    /**
+     * Проверка: в ответ пришло число от 0 до длинны массива с названием тестов и упражнений (ключая приветствие и заключение)
+     *    если да -> обновляются данные в sessionStorage и заполняется прогресс
+     *    если нет -> выскакивает попап с присланным текстом ошибки
+     */
     if (Number(res) >= 0 && Number(res) <= testsNames.length) {
       newListName = res == "Заключение" ? "Заключение" : testsNames[res];
       sessionStorage.setItem("listName", newListName);
       sessionStorage.setItem("listIndex", Number(res));
       setProgress();
+
+      const needsInstructions = checkForInstructions(newListName);
+
+      /**
+       * Проверка: если не нужна инструкция и у кнопки нет класса, который скрывает ее -> то он добавляется и убирается класс с анимацией
+       */
+      if (
+        !needsInstructions &&
+        !instrOpenBtn.classList.contains("instrBtn_hide")
+      ) {
+        instrOpenBtn.classList.remove("instrBtn_animation");
+        instrOpenBtn.classList.add("instrBtn_hide");
+      }
+
     } else {
       showPopup(res, true);
     }
 
   } catch (err) {
-
-    // alert(`submitTest 
-    // catch
-    // err:
-    // ${err}
-    // err.message:
-    // ${err.message}`);
-
     const btnText = getBtnText(listName);
-    // alert(`btnText ${btnText}`);
     submitTestBtn.textContent = btnText;
-
-    // showPopup("Что-то пошло не так. Подождите и попробуйте снова.", true);
-
-    // console.log(err);
-    // console.log(err.message);
-    // alert("Что-то пошло не так");
-    // alert("мы в catch");
-
-    // sessionStorage.setItem("listName", newListName);
 
   } finally {
-    console.log("закончили");
-
+    instrOpenBtn.disabled = false;
     submitTestBtn.disabled = false;
 
-    const btnText = newListName ? getBtnText(newListName) : getBtnText(listName);
+    // текст кнопки меняется в зависимости от номера листа, если в newListName есть данные - то передаются они, если нет - то старые данные
+    const btnText = newListName
+      ? getBtnText(newListName)
+      : getBtnText(listName);
     submitTestBtn.textContent = btnText;
-   
-    // const btnText = getBtnText(newListName);
-    // submitTestBtn.textContent = btnText;
+
     toggleTableBlock();
   }
 }
 
-
+/**
+ * Функция проверки наличия данных в sessionStorage. Если есть - открывается тестирование, если каких-то данных не хватает - нет
+ */
 function checkSessionStorage() {
   const url = sessionStorage.getItem("url");
   const email = sessionStorage.getItem("email");
@@ -277,6 +377,12 @@ function checkSessionStorage() {
     setData(email, listName, url);
 }
 
+/**
+ * Функция установки всех значений: устанавливается в iframe ссылка на таблицу, почта проставляется в шапку, по названию теста определяется текст на кнопке. Также переключаются блоки, проставляется прогресс и идет проверка на нужность инструкции
+ * @param {string} email - почта студента
+ * @param {string} listName - номер или название теста или упражнения
+ * @param {string} url - ссылка на таблицу
+ */
 function setData(email, listName, url) {
   tableTest.src = url;
   studentEmail.textContent = email;
@@ -286,8 +392,12 @@ function setData(email, listName, url) {
 
   setProgress();
   toggleSctions();
+  checkForInstructions(listName);
 }
 
+/**
+ * Функция заполнения шкалы прогресса
+ */
 function setProgress() {
   const listIndex = sessionStorage.getItem("listIndex");
   const amountOfTests = sessionStorage.getItem("amountOfTests");
@@ -296,7 +406,11 @@ function setProgress() {
   progress.style.width = `${width}%`;
 }
 
-
+/**
+ * Функция определяет текст кнопки по номеру теста
+ * @param {string} listName - название или номер теста или упражнения 
+ * @returns string текст для кнопки
+ */
 function getBtnText(listName) {
   let btnText;
 
@@ -321,6 +435,11 @@ function getBtnText(listName) {
   return btnText;
 }
 
+/**
+ * Функция определяет текст кнопки в момент отправки теста по его номеру
+ * @param {string} listName - название или номер теста или упражнения 
+ * @returns string текст для кнопки
+ */
 function getBtnText_onSubmit(listName) {
   let btnText;
 
@@ -347,5 +466,8 @@ submitTestBtn.addEventListener("click", submitTest);
 emailForm.addEventListener("submit", openTests);
 
 popupBtn.addEventListener("click", closePopup);
+
+instrCloseBtn.addEventListener("click", closeInstruction);
+instrOpenBtn.addEventListener("click", showInstruction);
 
 checkSessionStorage();
